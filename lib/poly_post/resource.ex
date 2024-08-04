5838f3f9fbfbb3edd0d2b133e3a58fbdcfbc8255
Angelo Lakra <angelo.lakra@gmail.com>
2024-08-03 20:32:51 -0600

    Added PolyPost.Resource behavior to define `build/3` callback (#9)
    * Added build callback to show how to transform a file with markdown into a structured map
    * Added some metadata

---
+++ lib/poly_post/resource.ex
@@ -0,0 +1,13 @@
+defmodule PolyPost.Resource do
+  @moduledoc """
+  A behavior used to transform a resource (like a markdown file) into structured content.
+  """
+  @moduledoc since: "0.1.0"
+
+  @type key :: term()
+  @type content :: term()
+  @type tag :: atom()
+
+  @doc "Defines a way to return the desired term for the resource"
+  @callback build(reference :: String.t, metadata :: Keyword.t, body :: String.t) :: %{key: key(), content: content()}
+end
