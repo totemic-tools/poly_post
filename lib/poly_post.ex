de3be37aee39493d7edad49703a5a5c65416b62b
Angelo Lakra <angelo.lakra@gmail.com>
2024-08-04 21:36:32 -0600

    Added app-level supervision tree with registry

--- lib/poly_post.ex
+++ lib/poly_post.ex
@@ -1,2 +1,27 @@
 defmodule PolyPost do
+  use Supervisor
+
+  # API
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  # Callbacks
+
+  def child_spec(_) do
+    %{
+      id: __MODULE__,
+      type: :supervisor,
+      start: {__MODULE__, :start_link, []}
+    }
+  end
+
+  def init(_) do
+    children = [
+      {Registry, [keys: :unique, name: PolyPost.Registry]}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 end
