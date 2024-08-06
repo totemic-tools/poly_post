52f64e931c59e91381b59e3d4d73ac6fbc7be609
Angelo Lakra <angelo.lakra@gmail.com>
2024-08-05 20:21:28 -0600

    Added module and function documentation

--- lib/poly_post/depot.ex
+++ lib/poly_post/depot.ex
@@ -1,4 +1,8 @@
 defmodule PolyPost.Depot do
+  @moduledoc """
+  A process to manage the content for a resource (articles, etc.).
+  """
+
   use GenServer
 
   alias PolyPost.Resource
@@ -7,26 +11,42 @@ defmodule PolyPost.Depot do
 
   # API
 
+  @doc """
+  Starts the Depot process with a given name to represent the
+  resource.
+  """
   @spec start_link(name()) :: GenServer.on_start()
   def start_link(name) do
     GenServer.start_link(__MODULE__, name, name: via(name))
   end
 
+  @doc """
+  Removes all the content related to the resouce.
+  """
   @spec clear(name()) :: :ok
   def clear(name) do
     GenServer.call(via(name), :clear)
   end
 
+  @doc """
+  Finds specific content by a key for the resource.
+  """
   @spec find(name(), Resource.key()) :: Resource.content()
   def find(name, key) do
     GenServer.call(via(name), {:find, key})
   end
 
+  @doc """
+  Gets all content for a resource.
+  """
   @spec get_all(name()) :: [{Resource.key(), Resource.content()}]
   def get_all(name) do
     GenServer.call(via(name), :get_all)
   end
 
+  @doc """
+  Inserts content for a resource via a key
+  """
   @spec insert(name(), Resource.key(), Resource.content()) :: :ok
   def insert(name, key, content) do
     GenServer.cast(via(name), {:insert, key, content})
