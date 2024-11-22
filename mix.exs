c1244a6ec1fba2be024c1d278bf58821ff0cf803
Angelo Lakra <angelo.lakra@gmail.com>
2024-11-22 07:34:07 -0700

    Added excoveralls as a dev/test dependency

--- mix.exs
+++ mix.exs
@@ -15,6 +15,14 @@ defmodule PolyPost.MixProject do
       deps: deps(),
       description: description(),
       package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ],
       source_url: @source_url,
       homepage_url: @source_url
     ]
@@ -37,15 +45,19 @@ defmodule PolyPost.MixProject do
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
 
       # Dev + Test
+      {:castore, "~> 1.0", only: [:dev, :test]},
       {:jason, "~> 1.4", only: [:dev, :test]},
       {:toml, "~> 0.7", only: [:dev, :test]},
-      {:yaml_elixir, "~> 2.1", only: [:dev, :test]}
+      {:yaml_elixir, "~> 2.1", only: [:dev, :test]},
+
+      # Test
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
   defp description do
     """
-    A publishing engine with markdown and code highlighting support.
+    A publishing engine with metadata, markdown and code highlighting support.
     """
   end
 
