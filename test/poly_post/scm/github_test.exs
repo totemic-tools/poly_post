3b9bf76af75e53d909eb7158c9a12898e77e7535
Angelo Lakra <angelo.lakra@gmail.com>
2024-11-25 10:26:18 -0700

    Tested PolyPost.Scm.Github.expand_repo

--- test/poly_post/scm/github_test.exs
+++ test/poly_post/scm/github_test.exs
@@ -1,9 +1,13 @@
 defmodule PolyPost.Scm.GithubTest do
   use ExUnit.Case, async: false
+  use Mneme
+
+  alias PolyPost.Scm.Github
 
   describe "expand_repo/1" do
     test "expands github repo shorcut to full URI" do
-      flunk "TBA"
+      auto_assert "https://github.com/totemic-tools/poly_post.git" <-
+        Github.expand_repo("totemic-tools/poly_post")
     end
   end
 end
